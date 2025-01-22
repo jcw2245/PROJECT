70690d18f3f2d50d2e5e1569f23bd4d9fded3374
jcw2245 <jcw2245@columbia.edu>
2025-01-21 21:23:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,9 @@ def hello():
 @app.route("/parks")
 def parks():
     df = pd.read_csv("data.csv")
-    parks = df.to_dict('records') 
     
     return render_template(
         'index.html',
-        parks=parks) 
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
